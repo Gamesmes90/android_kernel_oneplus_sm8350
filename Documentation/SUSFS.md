be729dd61388a618784edff995d5aca2d6d394e1
Gamesmes90 <gamesmes90@gmail.com>
2026-07-30 08:18:35 +0200

    SUSFS patching workflow revamped

--- Documentation/SUSFS.md
+++ Documentation/SUSFS.md
@@ -7,15 +7,24 @@ Commit: [17dbf64255b576c7b8ae5ab6950ed8761e1b2f63](https://github.com/Gamesmes90
 
 Patch (exact commit): [susfs_patch_to_5.4.patch](https://github.com/JackA1ltman/NonGKI_Kernel_Build_2nd/blob/cea906a15e59d4fc2913289720b3be5400b3b934/Patches/Patch/susfs_patch_to_5.4.patch)
 
-The actual patch used to add SUSFS is tracked within this repository [here](../susfs_patch_to_5.4.patch)
-
 ### Notes
 
-The patch workflow looks like this
-- [update-susfs-patch.sh](../update-susfs-patch.sh) merges the remote patch with the local one
-- [apply-susfs-patch.sh](../apply-susfs-patch.sh) applies the new patch reverting the old one
-
-This is necessary to make sure the patched code goes where it should since the upstream patch expects the patches to occur in lines that do not match with the ones of this repository's files.
+The patch workflow is managed by [susfs_patch.sh](../scripts/susfs/susfs_patch.sh)
+```
+          Download patch
+               |
+   checkout [detached HEAD] <- [upstream branch]
+               |
+           apply patch
+               |
+    fix rejected hunks with wiggle
+               |
+             commit
+               |
+     checkout [working branch]
+               |
+     cherry pick [detached commit hash]
+```
 
 ### Sources
 [NonGKI_Kernel_Build_2nd](https://github.com/JackA1ltman/NonGKI_Kernel_Build_2nd) - Patch
---
+++ scripts/susfs/susfs_patch.sh
@@ -0,0 +1,91 @@
+#!/bin/bash
+# SPDX-License-Identifier: GPL-2.0
+# This script applies the SUSFS patch for kernel version 5.4
+# Workflow:
+#          Download patch
+#               |
+#   checkout [detached HEAD] <- [upstream branch]
+#               |
+#           apply patch
+#               |
+#    fix rejected hunks with wiggle
+#               |
+#             commit
+#               |
+#     checkout [working branch]
+#               |
+#     cherry pick [detached commit hash]
+#
+# This way 95% of the patching is done automatically
+# The only manual work remaining would be fixing any merge conficts and adjusting the code if needed
+#
+# Patch: https://github.com/JackA1ltman/NonGKI_Kernel_Build_2nd/blob/mainline/Patches/Patch/susfs_patch_to_5.4.patch
+
+DIR=scripts/susfs
+PATCH_FILE=susfs_patch_to_5.4.patch
+UPSTREAM=lineage-23.2
+TARGET=android-16
+
+set -euo pipefail
+
+if [[ -z "${1:-}" ]] then
+    echo "No commit message provided."
+    exit 1
+fi
+
+# Download patch from HEAD
+curl -o new-patch.patch https://raw.githubusercontent.com/JackA1ltman/NonGKI_Kernel_Build_2nd/refs/heads/mainline/Patches/Patch/$PATCH_FILE
+
+# Save new hash (for documentation)
+echo  $(curl -s \
+        "https://api.github.com/repos/JackA1ltman/NonGKI_Kernel_Build_2nd/commits?path=Patches/Patch/susfs_patch_to_5.4.patch&sha=mainline&per_page=1" \
+        | jq -r '.[0].sha') > $DIR/patch_hash
+
+# Detach with upstream branch as base
+git checkout --detach $UPSTREAM
+
+# Apply the patch
+git apply --reject --whitespace=fix new-patch.patch || true
+
+# Check for rej files and merge them
+rejs_found=0
+while IFS= read -r -d '' rej; do
+    rejs_found=1
+    orig="${rej%.rej}"
+    echo "Found reject file. Running wiggle on: $orig"
+    
+    if [ -f "$orig" ]; then
+        if wiggle --replace "$orig" "$rej"; then
+            echo "   Wiggle merged $orig successfully"
+            rm -f "$rej" "$orig.porig"
+        else
+            echo "   Wiggle failed to merge $orig"
+            rm -f "$orig.porig"
+        fi
+    else
+        echo "   Error: The original file $orig for $rej does not exist"
+    fi
+done < <(find . -type f -name "*.rej" -print0)
+
+# Output if no rej files are found
+if [ "$rejs_found" -eq 0 ]; then
+    echo "No .rej files found (the patch has been 100% applied or it failed from the start)."
+fi
+
+# Commit
+git add -u && git add '*.h' '*.c' 'Makefile'
+git commit -am "$1"
+
+# Get commit hash
+HASH=$(git rev-parse --short HEAD)
+
+# Delete patch file
+rm new-patch.patch
+
+# Checkout to working branch
+git checkout $TARGET
+
+# Cherry pick the detached commit
+git cherry-pick $HASH
+
+exit 0
